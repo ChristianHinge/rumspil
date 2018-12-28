19dadd1fb54a5f2dbaee64b63883edc5b8ed7264
unknown <Philip Hinge>
2018-12-28 09:47:05 +0100

    version 2

---
+++ RumSpil/Assets/Scripts/PlayerScripts/Weapon/Gun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+    public float damage = 10f;
+    public Transform shootPoint;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        
+    }
+    void Shoot()
+    {
+        if (Physics.Raycast(shootPoint.position, shootPoint.forward, out RaycastHit hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            
+        }
+    }
+
+}
+
