8e547b5b49c164ce256a4920d5d52c04e6fe8f8e
unknown <Philip Hinge>
2018-12-28 17:01:26 +0100

    laser

--- RumSpil/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ RumSpil/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 10f;
+    public float moveSpeed = 5f;
     public float turnSpeed = 20f;
     public float rollSpeed = 50f;
+    public float boostSpeed = 5f;
+   
+
     Rigidbody rb;
 
-
+    
     private void start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -18,25 +21,21 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Thrust();
         Turn();
-        
+        Thrust();
+
     }
-    
     void Turn()
     {
-        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");   
-        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch"); 
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
         float roll = rollSpeed * Time.deltaTime * Input.GetAxis("Roll");
-        transform.Rotate(-pitch,yaw,-roll);
-                
-
+        transform.Rotate(-pitch, yaw, -roll);
     }
 
     void Thrust()
     {
-        transform.position += transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position += transform.forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
     }
-   
-                       
-}
+}    
+
