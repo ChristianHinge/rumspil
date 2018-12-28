311b6c091c08014eddd152484d9288e6f9308ef3
unknown <Philip Hinge>
2018-12-28 10:55:26 +0100

    Version 3

---
+++ RumSpil/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 defaultDistance = new Vector3(0f,2f,-10f);
+    [SerializeField] float distanceDamp = 10f;
+    [SerializeField] float rotationDamp = 10f;
+    public Vector3 velocity = Vector3.one;
+
+
+    private void Awake()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        
+        Vector3 toPos = target.position + (target.rotation * defaultDistance);
+        Vector3 curPos = Vector3.Lerp(transform.position, toPos, distanceDamp * Time.deltaTime);
+        transform.position = curPos;
+
+
+        Quaternion toRot = Quaternion.LookRotation(target.position - transform.position, target.up);
+        Quaternion curRot = Quaternion.Slerp(transform.rotation, toRot, rotationDamp * Time.deltaTime);
+        transform.rotation = curRot;
+
+    }
+    
+}
