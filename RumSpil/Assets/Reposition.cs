6da8a2a186d9dc8e28a50ad8efd4b754e9ddd3df
unknown <Philip Hinge>
2018-12-29 14:45:56 +0100

    playercamera

---
+++ RumSpil/Assets/Reposition.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    public Transform ParentT;
+    public Transform myT;
+
+    private void Start()
+    {
+        myT.position = ParentT.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RepositionPlayer();
+    }
+
+    void RepositionPlayer()
+    {
+        if (ParentT.position != myT.position)
+        {
+            myT.position = ParentT.position;
+        }
+    }
+}
+    
